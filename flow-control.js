5ff690c91f367ba284758ac5cb4205a5cef32b62
Matias Rebori <martinezrebori@gmail.com>
2019-07-05 18:04:05 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -30,7 +30,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  return (age<= 19 && age>=13) ? "You are a teenager!" : "You are not a teenager!";
+  return (age<= 19 && age>=13) ? "You are a teenager!" : "You are not a teenager";
 }
 
 function switchAge(age) {
